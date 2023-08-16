e82e8ff0ed6914689e757fc9541a2ae2f4acf190
Vali Zairov <zairov@gmail.com>
2023-08-15 23:35:28 -0700

    Initial commit

---
+++ Gomoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gomoku
+//
+//  Created by Vali Zairov on 8/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
